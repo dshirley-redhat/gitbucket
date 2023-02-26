b7444e06280ed75f97a68e4fdf8d52e1a3d7ae9d
Derek Shirley <dshirley@redhat.com>
2023-02-26 12:44:15 -0500

    removed more hardcoded values

--- import_repo.sh
+++ import_repo.sh
@@ -3,9 +3,6 @@
 set -x
 
 TMP=tmp_repo
-gb_userpass='root:root'
-gitbucket_url="gitbucket.apps.dshirley1ipi.vmware.tamlab.rdu2.redhat.com"
-gitbucket_user="root"
 
 cd /tmp
 
@@ -17,11 +14,11 @@ for repo in `echo $import_repos`; do
     git clone --mirror https://github.com/dshirley-redhat/$repo $TMP
 
     # create the repo on gitbucket
-    http --verify=no --ignore-stdin --auth $gb_userpass POST https://$gitbucket_url/api/v3/orgs/$gitbucket_user/repos name=$repo
+    http --verify=no --ignore-stdin --auth $gitbucket_user:$gitbucket_password POST https://$gitbucket_url/api/v3/orgs/$gitbucket_user/repos name=$repo
 
     # push to gitbucket
     pushd $TMP
-    git remote set-url origin https://root:root@$gitbucket_url/$gitbucket_user/$repo.git
+    git remote set-url origin https://$gitbucket_user:$gitbucket_password@$gitbucket_url/$gitbucket_user/$repo.git
     git -c http.sslVerify=false push origin --mirror -f
 
     popd && rm -rf $TMP
