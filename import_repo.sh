792df38d30142211e07a6aeb36c74bb52974d15f
Derek Shirley <dshirley@redhat.com>
2023-02-27 11:50:40 -0500

    added script to create webhook

--- import_repo.sh
+++ import_repo.sh
@@ -3,7 +3,8 @@
 set -x
 
 TMP=tmp_repo
-
+developer_argo_webhook_url="developer-gitops-server-developer-gitops.apps.dshirley1ipi.vmware.tamlab.rdu2.redhat.com"
+ 
 cd /tmp
 
 for repo in `echo $import_repos`; do
@@ -14,8 +15,13 @@ for repo in `echo $import_repos`; do
     git clone --mirror https://github.com/dshirley-redhat/$repo $TMP
 
     # create the repo on gitbucket
-    http --verify=no --ignore-stdin --auth $gitbucket_user:$gitbucket_password POST https://$gitbucket_url/api/v3/orgs/$gitbucket_user/repos name=$repo
+    http --verify=no --ignore-stdin --auth $gitbucket_user:$gitbucket_password POST \
+        https://$gitbucket_url/api/v3/orgs/$gitbucket_user/repos name=$repo
 
+    http --verify=no --ignore-stdin --auth $gitbucket_user:$gitbucket_password POST \
+        https://$gitbucket_url/api/v3/orgs/$gitbucket_user/$repo/hooks \
+        '{"name":"web","active":true,"events":["push","pull_request"],"config":{"url":'https://$developer_argo_webhook_url/api/webhook',"content_type":"json","insecure_ssl":"0"}}'
+ 
     # push to gitbucket
     pushd $TMP
     git remote set-url origin https://$gitbucket_user:$gitbucket_password@$gitbucket_url/$gitbucket_user/$repo.git
